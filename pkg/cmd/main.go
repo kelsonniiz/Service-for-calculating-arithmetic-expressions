2f4d4d8a9a537562cc4f51b8910f2d6ace3ea47d
kelsonniiz <kelsonniiz@gmail.com>
2024-12-17 20:58:22 +0500

    main.go

--- pkg/cmd/main.go
+++ pkg/cmd/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/Cool-Andrey/Calculating/pkg/internal/application"
+import "github.com/kelsonniiz/Service-for-calculating-arithmetic-expressions/pkg/internal/application"
 
 func main() {
 	app := application.New()
